01ccd8ea2aef71b9f89ef2a04faeac5fe5ae7a92
miketoreno88 <toreno.m@mail.ru>
2022-12-21 14:55:52 +0300

    modified:   uncode.py

--- uncode.py
+++ uncode.py
@@ -1,16 +1,16 @@
 # ru
-# abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-# word = "стретвоокуф"
+abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+word = "стретвоокуф"
 
-# def code(text, step):
-#     decryption = ""
-#     for a in text:
-#         for item in range(len(abc)):
-#             if a == str(abc[item]):
-#                 decryption += abc[item-step]
-#     return(decryption)
+def code(text, step):
+    decryption = ""
+    for a in text:
+        for item in range(len(abc)):
+            if a == str(abc[item]):
+                decryption += abc[item-step]
+    return(decryption)
 
-# print(code(word, 2))
+print(code(word, 2))
 
 #eu
 abc = "abcdefghijklmnopqrstuvwxyz"
